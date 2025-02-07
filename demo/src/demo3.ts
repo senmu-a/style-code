d45a04b548c357c9f974d07650d8d739dba38801
senmu <951967917@qq.com>
2025-02-07 21:29:51 +0800

    feat: 树的深度遍历

--- demo/src/demo3.ts
+++ demo/src/demo3.ts
@@ -117,3 +117,72 @@ function stackLast(node: TreeNode | null) {
     }
   }
 }
+
+// 先序遍历
+function stackFirst1(root: TreeNode | null) {
+  const stack = [root];
+  while (stack.length) {
+    const node = stack.pop(); // 根节点出栈
+    console.log(node.cur);
+    node.right && stack.push(node.right); // 右节点入栈
+    node.left && stack.push(node.left); // 左节点入栈
+  }
+}
+
+/**
+ * 中序遍历
+ * 顺序：左 -> 根 -> 右
+ * 先把左子树的根节点和左节点入栈，然后出栈，再把右节点入栈
+ * 步骤：
+ *  1. 创建一个栈 stack，创建一个 node 节点指向 root
+ *  2. 遍历 stack 或者 node 节点有内容
+ *  3. 判断 node 是否有内容，如果有的话就把 node 入栈，然后 node 指向 node 的左节点（此步骤是处理左子树，不断向左走）
+ *  4. 如果 node 为空，说明左子树已经走到头了，此时就要出栈，然后再把 node 的右节点入栈
+ *  5. 重复 3-4 步骤，直到 stack 为空并且 node 为空
+ */
+function stackMiddle1(root: TreeNode | null) {
+  const stack: TreeNode[] = [];
+  let node = root;
+  while (stack.length || node) {
+    if (node) {
+      stack.push(node);
+      node = node.left;
+    } else {
+      node = stack.pop();
+      console.log(node.cur);
+      node = node.right;
+    }
+  }
+}
+
+/**
+ * 后序遍历
+ * 顺序：左 -> 右 -> 根
+ * 按照顺序来看，也需要先处理左子树，而每次我们拿到的节点是“根节点”，那我们就将左子树中的这些根节点依次入栈，当处理到左子树的最后一个节点时，我们就要出栈，然后再判断处理右侧节点与根节点
+ * 步骤：
+ *  1. 创建一个栈 stack，创建一个 node 节点指向 root，创建一个 last 节点指向 null（关键！！！因为要保证右节点比根节点早输出）
+ *  2. 遍历 stack 或者 node 节点有内容
+ *  3. 判断 node 是否有内容，如果有的话就把 node 入栈，然后 node 指向 node 的左节点（此步骤是处理左子树，不断向左走）
+ *  4. 如果 node 为空，说明左子树已经走到头了，此时就要出栈，并将 node 赋值为出栈的节点，判断该节点是否有右节点，如果有的话就把该节点入栈，如果没有的话就输出该节点
+ *  5. 重复 3-4 步骤，直到 stack 为空并且 node 为空
+ */
+function stackLast1(root: TreeNode | null) {
+  const stack: TreeNode[] = [];
+  let node = root;
+  let last: TreeNode | null = null;
+  while (stack.length || node) {
+    while (node) {
+      stack.push(node);
+      node = node.left;
+    }
+    node = stack.pop();
+    if (!node.right || node.right === last) {
+      console.log(node.cur);
+      last = node;
+      node = null;
+    } else {
+      stack.push(node);
+      node = node.right;
+    }
+  }
+}
